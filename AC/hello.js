d4a1651789bc8be8b1ae833dc9a5a4156581ea81
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-11-04 15:10:29 +0000

    Variable Testing

--- AC/hello.js
+++ AC/hello.js
@@ -1,1 +1,23 @@
 console.log("Hellow World");
+/*let a=null;
+let b = 345;
+let c = true;
+let d = BigInt("567")+BigInt("3");
+let f = Symbol("I Am a nice symbol");
+console.log(a,b,c,d,f);
+console.log(typeof c);*/
+// Non-Primitive 
+console.log("Operates in JS");
+  let a=10;
+  let b=4;
+  console.log("a+b=",a+b);
+  console.log("a-b=", a-b);
+  console.log("a/b=",a/b);
+  console.log("a**b (power in case )=", a**b );
+  console.log("a%b = ", a%b);
+  console.log("a++ = ", a++);
+  console.log("Print ", a);
+  console.log("a-- = ", a--);
+  console.log(" a ", a);
+  
+
