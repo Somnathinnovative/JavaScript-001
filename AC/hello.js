0c4c28a093fedb5fa7946fbd904b1d3749c6e63e
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-11-05 05:20:14 +0000

    new

--- AC/hello.js
+++ AC/hello.js
@@ -1,4 +1,4 @@
-console.log("Hellow World");
+/*console.log("Hellow World");
 /*let a=null;
 let b = 345;
 let c = true;
@@ -7,7 +7,7 @@ let f = Symbol("I Am a nice symbol");
 console.log(a,b,c,d,f);
 console.log(typeof c);*/
 // Non-Primitive 
-console.log("Operates in JS");
+/*console.log("Operates in JS");
   let a=10;
   let b=4;
   console.log("a+b=",a+b);
@@ -19,5 +19,13 @@ console.log("Operates in JS");
   console.log("Print ", a);
   console.log("a-- = ", a--);
   console.log(" a ", a);
-  
-
+  // Promt -> Ask Question
+  // Alert -> give Alert message
+ // let aa = prompt(" Hey what's your age "); // error 
+  aa= Number.parseInt(a); // converting
+  console.log(typeof a);*/
+const accountID = 14453;
+let accountIDEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountIDEmail="hc@dhc.com";
+accountPassword=2121;
