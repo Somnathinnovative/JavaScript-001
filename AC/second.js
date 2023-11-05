87ebd69ee582193c434101ec33d7f86234c96a14
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-11-05 15:01:49 +0000

    javascript

---
+++ AC/second.js
@@ -0,0 +1,16 @@
+// Type Conversion
+//let score = 33;
+/*console.log(typeof score);
+ console.log(typeof (score));
+ let valuInNumber = Number(score);
+ console.log(typeof valuInNumber);
+ console.log(typeof valuInNumber);
+ console.log(valuInNumber);*/
+
+// let score = undefined;
+let isLoggedIn=1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+let SomeNumber = 33;
+let stringNumber = String(SomeNumber);
+console.log(stringNumber);
