ca40914e2b19aa136c779ce7dc837d47595ed22b
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-11-19 03:30:05 +0000

    Commit

---
+++ AC/Obj.js
@@ -0,0 +1,12 @@
+let UserOne = {
+    email : "User@google.com",
+    upi : "User@ybl",
+}
+
+console.log(UserOne.email);
+
+const name = "hitesh";
+const re_count = 50;
+console.log(name + re_count + "value");
+
+console.log(`Hello my name ${name} and my repo count ${re_count}`);
