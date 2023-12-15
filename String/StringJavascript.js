bb9b33119bdec3c40e610acdc1923c343f556cbf
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-12-15 05:18:12 +0000

    String

---
+++ String/StringJavascript.js
@@ -0,0 +1,11 @@
+// Now a days using string interpolation and placeholds 
+
+const name = "hitesh";
+const repocount = 50;
+console.log(name + repocount + "value");
+
+// interpolation and placeholds 
+console.log(`Hello my name is ${name} and my nepo count is ${repocount}`);
+// declaration String 
+const gameName = new String('hitesh');
+
