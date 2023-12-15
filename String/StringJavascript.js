d3f376fc0c730747a9fe40ff2311fda6c4d08052
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-12-15 05:38:49 +0000

    String interpolation

--- String/StringJavascript.js
+++ String/StringJavascript.js
@@ -8,4 +8,12 @@ console.log(name + repocount + "value");
 console.log(`Hello my name is ${name} and my nepo count is ${repocount}`);
 // declaration String 
 const gameName = new String('hitesh');
+console.log(gameName[0]);
+console.log(gameName[1]);
+console.log(gameName[2]);
+console.log(gameName[3]);
+console.log(gameName[4]);
+console.log(gameName[5]);
+
+
 
