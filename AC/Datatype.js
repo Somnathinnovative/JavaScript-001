77d4605872fe7184c2d83ed0ff41b234500eba86
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-12-15 04:46:41 +0000

    All Data Types

---
+++ AC/Datatype.js
@@ -0,0 +1,37 @@
+
+    // All Datatypes of Javascript
+const score = 100;
+const ScoreValue=100.3;
+const isLoggedIn = false;
+const outSideTemp=null;
+let UserEmail;
+
+const id = Symbol('123');
+const anotherID = Symbol('123');
+//console(id===anotherID);
+const bigNumber = 34564789n;
+const heros= ["shaktiman","naagraj","doga"];
+let myObj = {
+    name : "somnath",
+    age : 35
+}
+
+const myFunction = function() {
+    console.log("Hellow world");
+}
+
+/*
+        Stack and heap memory
+        memory --->  Stack (Primitive)
+        Heap (Non-Primitive)
+        let myYoutubeername = "hiteshchoudery.com";
+        Non-Primitive----- By Reference 
+*/
+
+/************
+ * 
+ * ######### Primitive datatypes : String, Number , Boolean , Null, Undefind , Symbol , BigInt
+ *                                                                                          ########
+ * 
+ * ********* */
+
