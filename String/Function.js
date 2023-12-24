298609f1442a3f4aa39e319c68fbc69f4e3cfd7f
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-12-24 14:45:15 +0000

    commit

---
+++ String/Function.js
@@ -0,0 +1,19 @@
+//Functions are one of the fundamental building blocks in JavaScript.
+function SayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+}
+
+SayMyName();
+
+function addTwoNumbers(number1, number2)
+{
+    console.log(number1+number2);
+}
+addTwoNumbers(3,null);
+addTwoNumbers(50, 25);
+//addTwoNumbers("3",4);
+
