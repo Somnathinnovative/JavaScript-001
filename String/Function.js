6e415b5b1710ae037d0d12ebd43ad2e2bdfa5eb9
Somnathinnovative <116653248+Somnathinnovative@users.noreply.github.com>
2023-12-25 04:30:40 +0000

    investigation study

--- String/Function.js
+++ String/Function.js
@@ -15,5 +15,19 @@ function addTwoNumbers(number1, number2)
 }
 addTwoNumbers(3,null);
 addTwoNumbers(50, 25);
-//addTwoNumbers("3",4);
+addTwoNumbers("3",4);
+addTwoNumbers('3','4');
+addTwoNumbers(null,3);
+
+const result = addTwoNumbers(3,5); // No return value
+console.log("REsult :",result);
+//////////////////////////////
+function addTwoNumbers(number1,number2)
+{
+        let result =  number1+number2;
+        return result;
+}
+
+const result1=addTwoNumbers(6,4);
+console.log(result);
 
